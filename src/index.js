ff51b103251bce8a45f256c25e6e15004350bc7b
Ha Thu <tranhathu6989@gmail.com>
2022-06-06 11:28:01 +0200

    Routes and Controller

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes/index.routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
+//Middleware urlencoded để có thể xử lý dữ liệu từ client lên server qua form sublit html
+app.use(express.urlencoded({ extended: true }));
+//Middleware json để có thể xử lý dữ liệu từ code JVS qua thư viện code js đẻ submit: XMLHttp, fetch, axios, ...
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Template engine
 app.engine(
@@ -23,9 +30,8 @@ app.set("views", path.join(__dirname, "resources", "views"));
 // console.log("dirname : ", __dirname);
 // console.log("PATH   : ", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/routes/news.routes.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+const newsController = require("../app/controllers/NewsController");
+
+// Route luôn đọc từ trên xuống => "/" dưới cùng
+router.use("/:slug", newsController.show);
+router.use("/", newsController.index);
+
+module.exports = router;
