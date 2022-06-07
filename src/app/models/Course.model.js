bc9494fecaab00bb5beccea4a9de2549868069d7
Ha Thu <tranhathu6989@gmail.com>
2022-06-07 12:55:46 +0200

    List-Detail-Create Course

--- src/app/models/Course.model.js
+++ src/app/models/Course.model.js
@@ -1,27 +1,25 @@
 const mongoose = require("mongoose");
+
+//Loading
+const slug = require("mongoose-slug-generator");
+
+//Initialization
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
-const courseSchema = new Schema({
-  name: {
-    type: String,
-    maxlength: 255,
+const courseSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
   },
-  description: {
-    type: String,
-    maxlength: 600,
-  },
-  image: {
-    type: String,
-    maxlength: 255,
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updateAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 //mongoose.model('ModelName', mySchema);
 //Sang MongoDB Course sẽ đc tự động viết thường và số nhiều Course => courses
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 const newsRouter = require("./news.routes");
+const coursesRouter = require("./courses.routes");
 const siteRouter = require("./site.routes");
 
 function route(app) {
   app.use("/news", newsRouter);
+  app.use("/courses", coursesRouter);
   app.use("/", siteRouter);
 }
 
